be77862860bd662aafd4e9c35a65af85f9f6ed03
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-05-17 20:33:01 -0500

    Create Tesis.java

---
+++ src/main/java/comunicacion/Tesis.java
@@ -0,0 +1,78 @@
+package comunicacion;
+//import java.util.*;
+
+public class Tesis extends Escrito{
+	private String idea;
+	private String[] argumentos;
+	private String conclusion;
+	private String referencias;
+	private String interpretacion;
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.getOrigen()+
+				"\n"+this.getTitulo()+
+				"\n"+this.getAutor()+
+				"\n"+this.getPaginas()+
+				"\n"+this.idea+
+				"\n"+this.argumentos.length+
+				"\n"+this.conclusion+
+				"\n"+this.referencias;
+	}
+	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos,
+			String conclusion, String referencias, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.idea = idea;
+		this.argumentos = argumentos;
+		this.conclusion = conclusion;
+		this.referencias = referencias;
+		this.interpretacion = interpretacion;
+	}
+	@Override
+	public int palabrasTotales(int palabrasPagina) {
+		
+		return this.getPaginas()*palabrasPagina*5;
+		
+	}
+	@Override
+	public String interpretacion() {
+		return this.interpretacion;
+	}
+	
+	public String getIdea() {
+		return idea;
+	}
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+	public String[] getArgumentos() {
+		return argumentos;
+	}
+	public void setArgumentos(String[] argumentos) {
+		this.argumentos = argumentos;
+	}
+	public String getConclusion() {
+		return conclusion;
+	}
+	public void setConclusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+	public String getReferencias() {
+		return referencias;
+	}
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+}
+
+	
+	
