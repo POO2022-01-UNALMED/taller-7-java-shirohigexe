6c34cf3021aa52e96c3ea5e02bc945a69416e9ca
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-05-17 20:32:54 -0500

    Create Fabula.java

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,45 @@
+package comunicacion;
+
+public class Fabula extends Escrito{
+	String ensenanza;
+	String interpretacion;
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.getOrigen()+
+				"\n"+this.getTitulo()+
+				"\n"+this.getAutor()+
+				"\n"+this.getPaginas()+
+				"\n"+this.ensenanza;
+	}
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanza = ensenanza;
+		this.interpretacion = interpretacion;
+	}
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	@Override
+	public int palabrasTotales(int palabrasPagina) {
+		return this.getPaginas()*palabrasPagina*1;
+	}
+	@Override
+	public String interpretacion() {
+		// TODO Auto-generated method stub
+		return this.interpretacion;
+	}
+	
+	
+	
+
+}
